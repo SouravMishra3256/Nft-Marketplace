73ebbd27eb8a5ded4a921e34dd6e2496c0317d24
Sourav Mishra <sourav.mishra@squbix.com>
2022-07-08 10:40:45 +0530

    minor additions

--- screens/Home.js
+++ screens/Home.js
@@ -5,12 +5,17 @@ import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 import { COLORS, NFTData } from "../constants";
 const Home = () => {
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-      }}
-    >
+    <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
+
+      <View style={{flex: 1}}>
+        <View style={{zIndex: 0}}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
